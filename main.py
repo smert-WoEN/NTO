af2fce5ac9f024a1cb4b32150c063f11a1b14ce6
DmitriiDenisov <denisdmit03@gmail.com>
2021-12-01 23:27:26 +0300

    some strange

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from numpy import *
+import numpy
 
-print("Hello World!")
+f = open("0")
+a1 = numpy.array([f.readline().split()], dtype=str)
+for i in range(1023):
+    a1 = numpy.append(a1, [f.readline().split()], axis=0)
+
+print(len(a1))
+
+
