21b639e7ada42fe15c65ab6ef08f8bddce6c7316
DmitryDenisov <denisdmit03@gmail.com>
2021-12-02 17:21:29 +0300

    add strange

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import cv2
-import numpy
 import numpy as np
 
-if __name__ == '__main__':
-    def nothing(*arg):
-        pass
-
-a1 = (np.zeros((1024,1280,3), dtype=np.uint8))
-f = open("2")
+dst = (np.zeros((1024, 1280, 3), dtype=np.uint8))
 for i in range(1024):
-    x = f.readline().split()
+    x = input()
     for j in range(1280):
         a = [x[j][:2], x[j][2:4], x[j][4:]]
         b = [int(a[0], 16), int(a[1], 16), int(a[2], 16)]
-        a1[i][j] = b
-        #a1 = np.append(a1, [[b]], axis=1)
-        #print(a1)
-        #print([b]+ [b])
-        #a1 = np.append(a1, [[b] + [b]], axis=0)
-print(a1)
-while True:
-    cv2.imshow("HSV", a1)
-    ch = cv2.waitKey(5)
-    if ch == 27:
-        break
+        dst[i][j] = b
+
+hsvMinRed = np.array((0, 110, 0), np.uint8)
+hsvMaxRed = np.array((50, 255, 255), np.uint8)
+hsvMinBlue = np.array((65, 20, 0), np.uint8)
+hsvMaxBlue = np.array((140, 255, 255), np.uint8)
+
+img = cv2.GaussianBlur(dst, (5, 5), cv2.BORDER_DEFAULT)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsvRedFilter = cv2.inRange(hsv, hsvMinRed, hsvMaxRed)
+contourRed, img4 = cv2.findContours(hsvRedFilter, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+redContourDraw = img.copy()
+cv2.drawContours(image=redContourDraw, contours=contourRed, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
+print(len(contourRed))
+
+hsvBlueFilter = cv2.inRange(hsv, hsvMinBlue, hsvMaxBlue)
+contourBlue, img6 = cv2.findContours(hsvBlueFilter, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+blueContourDraw = img.copy()
+cv2.drawContours(image=blueContourDraw, contours=contourBlue, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
+
+print(len(contourBlue))
+if len(contourBlue) > len(contourRed):
+    print("B " + str(len(contourBlue)))
+elif len(contourRed) > len(contourBlue):
+    print("R " + str(len(contourRed)))
+else:
+    print("S " + str(len(contourRed)))
+
+
+
 
 #inputMat = np.genfromtxt("0", delimiter=" ", dtype=str)
 
--- openCVNode.py
+++ openCVNode.py
@@ -10,22 +10,34 @@ hsvMaxRed = np.array((50, 255, 255), np.uint8)
 hsvMinBlue = np.array((65, 20, 0), np.uint8)
 hsvMaxBlue = np.array((140, 255, 255), np.uint8)
 
-img = cv2.imread("assignments.jpg", cv2.IMREAD_COLOR)
+dst = cv2.imread("assignments.jpg", cv2.IMREAD_COLOR)
+img = cv2.GaussianBlur(dst, (5, 5), cv2.BORDER_DEFAULT)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 hsvRedFilter = cv2.inRange(hsv, hsvMinRed, hsvMaxRed)
 contourRed, img4 = cv2.findContours(hsvRedFilter, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 redContourDraw = img.copy()
-cv2.drawContours(image=redContourDraw, contours=contourRed, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+cv2.drawContours(image=redContourDraw, contours=contourRed, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
 print(len(contourRed))
 
 hsvBlueFilter = cv2.inRange(hsv, hsvMinBlue, hsvMaxBlue)
 contourBlue, img6 = cv2.findContours(hsvBlueFilter, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 blueContourDraw = img.copy()
-cv2.drawContours(image=blueContourDraw, contours=contourBlue, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+cv2.drawContours(image=blueContourDraw, contours=contourBlue, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
 
 print(len(contourBlue))
+if len(contourBlue) > len(contourRed):
+    print("B " + str(len(contourBlue)))
+elif len(contourRed) > len(contourBlue):
+    print("R " + str(len(contourRed)))
+else:
+    print("S " + str(len(contourRed)))
+
 while True:
     cv2.imshow("HSV", hsv)
+    cv2.imshow("dst", dst)
+    cv2.imshow("img", img)
     cv2.imshow("hsvRedFilter", hsvRedFilter)
     cv2.imshow("hsvBlueFilter", hsvBlueFilter)
     cv2.imshow("redContourDraw", redContourDraw)
@@ -33,5 +45,3 @@ while True:
     ch = cv2.waitKey(5)
     if ch == 27:
         break
-
-
