47651a717b2cf8c5eddb898e33d46eb3de213ea1
DmitryDenisov <denisdmit03@gmail.com>
2021-12-22 20:31:16 +0300

    add something3

--- parthTwo/checkOne/main.py
+++ parthTwo/checkOne/main.py
@@ -1,11 +1,15 @@
+import math
 import time
 
 import numpy as np
 import colorsys
 
 s = time.time()
+
+
 def convertor(x):
     return [int(x[:2], 16), int(x[2:4], 16), int(x[4:], 16)]
+
 
 hsvMinRed = np.array((1, 0, 0), np.uint16)
 hsvMaxRed = np.array((45, 255, 255), np.uint16)
@@ -26,14 +30,13 @@ a = np.array(sorted(a, key=lambda x: (-x[2])))
 h = []
 for i in range(len(a)):
     b = a[i]
-    hls = colorsys.rgb_to_hls(b[3]/255.0, b[4]/255.0, b[5]/255.0)
-    if b[2] > maxZ and 15 < hls[2] * 100 and 15 < hls[1] * 100 <= 85 and \
+    hls = colorsys.rgb_to_hls(b[3] / 255.0, b[4] / 255.0, b[5] / 255.0)
+    if b[2] > maxZ and (5 < hls[1] * 100 < 95 and 100 * abs(math.cos(hls[1] * 2 * math.pi)) < hls[2] * 100) and \
             (hsvMinRed[0] <= hls[0] * 360 < hsvMaxRed[0] or
              hsvMinRed2[0] <= hls[0] * 360 < hsvMaxRed2[0] or
              hsvMinBlue[0] <= hls[0] * 360 < hsvMaxBlue[0]):
         maxZ = b[2]
         h = hls
-
 
 colorS = ""
 if hsvMinRed[0] <= h[0] * 360 < hsvMaxRed[0] or hsvMinRed2[0] <= h[0] * 360 < hsvMaxRed2[0]:
