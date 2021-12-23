042648d66b0218e795c27f4948edd419f309e801
DmitryDenisov <denisdmit03@gmail.com>
2021-12-23 19:27:23 +0300

    add partTree

---
+++ parthTree/checkOne.py
@@ -0,0 +1,7 @@
+l1, l2, l3 = [float(x) for x in input().split()]
+angleL1 = [float(x) for x in input().split()]
+angleL2 = [float(x) for x in input().split()]
+angleL3 = [float(x) for x in input().split()]
+x, y = [float(x) for x in input().split()]
+print(l1, l2, l3)
+print(angleL1)
